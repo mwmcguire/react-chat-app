461ee7aca869a521472a4457afa7938d2f1c9db6
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2019-12-15 23:02:37 -0500

    completed app

---
+++ src/Store.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+export const CTX = React.createContext();
+
+const initState = {
+  general: [
+    { from: 'aaron', msg: 'hello' },
+    { from: 'arnold', msg: 'hello' },
+    { from: 'archer', msg: 'hello' }
+  ],
+  topic2: [
+    { from: 'aaron', msg: 'hello' },
+    { from: 'arnold', msg: 'hello' },
+    { from: 'archer', msg: 'hello' }
+  ]
+};
+
+function reducer(state, action) {
+  const { from, msg, topic } = action.payload;
+  switch (action.type) {
+    case 'RECEIVE_MESSAGE':
+      return {
+        ...state,
+        [topic]: [...state[topic], { from, msg }]
+      };
+    default:
+      return state;
+  }
+}
+
+let socket;
+
+function sendChatAction(value) {
+  socket.emit('chat message', value);
+}
+
+export default function Store(props) {
+  const [allChats, dispatch] = React.useReducer(reducer, initState);
+
+  if (!socket) {
+    socket = io('http://localhost:3001');
+    socket.on('chat message', function(msg) {
+      dispatch({ type: 'RECEIVE_MESSAGE', payload: msg });
+    });
+  }
+
+  const user = 'aaron' + Math.random(100).toFixed(2);
+
+  return (
+    <CTX.Provider value={{ allChats, sendChatAction, user }}>
+      {props.children}
+    </CTX.Provider>
+  );
+}
